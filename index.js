ec44083cf762c2a27c81a3acbac3c59a5f5f08bf
Lionel Bertrand <lionel.bertrand@ymail.com>
2015-10-13 11:13:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
-var gutil       = require('gulp-util'),
-    requirejs   = require('requirejs'),
-    PluginError = gutil.PluginError,
-    File        = gutil.File,
-    es          = require('event-stream')
+var gutil = require('gulp-util'),
+  requirejs = require('requirejs'),
+  _ = require('underscore'),
+  PluginError = gutil.PluginError,
+  File = gutil.File,
+  es = require('event-stream');
 
 // Consts
-const PLUGIN_NAME = 'gulp-requirejs';
+var PLUGIN_NAME = 'gulp-requirejs';
 
 
-module.exports = function(opts) {
+function rjs(opts) {
 
-    'use strict';
+  this.opts = opts;
 
-    if (!opts) {
-        throw new PluginError(PLUGIN_NAME, 'Missing options array!');
-    }
+  if (!this.opts) {
+    throw new PluginError(PLUGIN_NAME, 'Missing options array!');
+  }
 
-    if (!opts.out && typeof opts.out !== 'string') {
-        throw new PluginError(PLUGIN_NAME, 'Only single file outputs are supported right now, please pass a valid output file name!');
-    }
+  if (!this.opts.out && typeof this.opts.out !== 'string') {
+    throw new PluginError(PLUGIN_NAME, 'Only single file outputs are supported right now, please pass a valid output file name!');
+  }
 
-    if (!opts.baseUrl) {
-        throw new PluginError(PLUGIN_NAME, 'Pipeing dirs/files is not supported right now, please specify the base path for your script.');
-    }
+  if (!this.opts.baseUrl) {
+    throw new PluginError(PLUGIN_NAME, 'Pipeing dirs/files is not supported right now, please specify the base path for your script.');
+  }
 
-    // create the stream and save the file name (opts.out will be replaced by a callback function later)
-    var _s     = es.pause(),
-        _fName = opts.out;
+  // create the stream and save the file name (opts.out will be replaced by a callback function later)
+  this._s = es.pause();
+  this._fName = this.opts.out;
 
-    // just a small wrapper around the r.js optimizer, we write a new gutil.File (vinyl) to the Stream, mocking a file, which can be handled
-    // regular gulp plugins (i hope...).
-    
-    // try {
-        optimize(opts, function(text) {
-            _s.write(new File({
-                path: _fName,
-                contents: new Buffer(text)
-            }));
-        });
-    // } catch (err) {
-    //     _s.emit('error', err);
-    // }
+  // just a small wrapper around the r.js optimizer, we write a new gutil.File (vinyl) to the Stream, mocking a file, which can be handled
+  // regular gulp plugins (i hope...).
 
-    
+  this._optimize();
+  // try {
+  // optimize(opts, function(text) {
+  //   _s.write(new File({
+  //     path: _fName,
+  //     contents: new Buffer(text)
+  //   }));
+  // });
+  // } catch (err) {
+  //     _s.emit('error', err);
+  // }
 
-    // return the stream for chain .pipe()ing
-    return _s;
+
+
+  // return the stream for chain .pipe()ing
+  return _s;
 }
+
+rjs.prototype._optimize = function(){
+  optimize(this.opts, function(text) {
+    this._s.write(new File({
+      path: this._fName,
+      contents: new Buffer(text)
+    }));
+  });
+};
+
+rjs.prototype.setOption = function(options) {
+  _.extend(this.opts, options);
+  this._optimize();
+};
 
 // a small wrapper around the r.js optimizer
 function optimize(opts, cb) {
-    opts.out = cb;
-    opts.optimize = 'none';
-    requirejs.optimize(opts);
+  opts.out = cb;
+  opts.optimize = 'none';
+  requirejs.optimize(opts);
 }
+
+module.exports = rjs;
